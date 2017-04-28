90fa3ae5801fbb154a17f46d0b44e9b9f26bde76
garrettb82 <garrettb82@yahoo.com>
2017-04-27 22:28:31 -0500

    Initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: [
+        './src/index.jsx'
+    ],
+    output: {
+        path: __dirname,
+        publicPath: '/reactify',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015', 'stage-1']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    }
+};
